ef1c50a139f9af1baff6f35d9a5d1c0fda5484c1
Neel Smith <dnsmith.neel@gmail.com>
2018-05-07 06:34:02 -0400

    Add skeleton test

---
+++ shared/src/test/scala/TestTreebank.scala
@@ -0,0 +1,24 @@
+package edu.holycross.shot.aldt
+
+package edu.holycross.shot.skelly
+import org.scalatest.FlatSpec
+
+class TreebankTest extends FlatSpec {
+
+
+//class  TreebankTest extends Specification {
+/*
+  "A Histogram object" should {
+    "map strings to counts" in {
+      val fileSource = Source.fromURL(getClass.getResource("/18.12.1950.txt"))
+      val tokens = Tokenizer.tokenizeFile(fileSource)
+      val hist = Histo.histogram(tokens)
+      hist.values must have size(103)
+      hist.values.max must_== 5
+      hist.values.min must_== 1
+    }
+   }
+
+*/
+
+}
